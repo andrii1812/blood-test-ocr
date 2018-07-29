322f26bcb42a10aee6345e50dce5292f4f6239ea
Andrew <andrii1812@gmail.com>
2018-07-29 12:41:25 +0300

    building frontend using docker container

--- build/build-frontend.sh
+++ build/build-frontend.sh
@@ -1,2 +1,7 @@
+#! /bin/sh
+BUILD_PATH="$(realpath "../src/web/static/")"
+
+chmod +x "$BUILD_PATH/build-fresh.sh"
+echo $BUILD_PATH
 docker build -f Dockerfile.build -t ocr-frontend-build .
-docker run --rm -v "../src/web/static":/app ocr-frontend-build
+docker run -it -v $BUILD_PATH:/app ocr-frontend-build
--- src/web/static/build-fresh.sh
+++ src/web/static/build-fresh.sh
@@ -1,6 +1,14 @@
+
+echo "[build] cleaning before build"
 rm -rf node_modules/
 rm -rf .cache/
 rm -rf dist/
 
+echo "[build] building to /dist"
 yarn install
+npm rebuild node-sass
 yarn build
+
+echo "[build] removing build artifacts"
+rm -rf node_modules/
+rm -rf .cache/
